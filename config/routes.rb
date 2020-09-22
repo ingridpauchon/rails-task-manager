71a44563b3d17a5ffe2db05472ee3c40b49195cd
Ingrid Pauchon <ingridpauchon@gmail.com>
2020-09-22 12:28:08 +0200

    added new create edit update delete actions, bootstrap and refactoring left

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,18 @@ Rails.application.routes.draw do
 #Reading all tasks
 get '/tasks', to: 'tasks#index'
 
-#reading details of one task
-get 'tasks/:id', to: 'tasks#show'
+#adding a new task
+get '/tasks/new', to: 'tasks#new'
+post '/tasks', to: 'tasks#create'
 
+#reading details of one task
+get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+#edit a task
+get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+patch 'tasks/:id', to: 'tasks#update'
+
+#delete a task
+delete 'tasks/:id', to: 'tasks#destroy', as: 'delete'
 
 end
