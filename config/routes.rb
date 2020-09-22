089d952c6cb17d55f13890e2419e98dbfa46e552
Ingrid Pauchon <ingridpauchon@gmail.com>
2020-09-22 11:27:18 +0200

    created model, migrated, completed index action, working on show details

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+#Reading all tasks
+get '/tasks', to: 'tasks#index'
+
+#reading details of one task
+get 'tasks/:id', to: 'tasks#show'
+
+
 end
